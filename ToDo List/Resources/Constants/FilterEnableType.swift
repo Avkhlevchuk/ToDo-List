675491bfe91a249daa9a5d1a76cde9c6fbe5048a
avkhlevchuk <avkhlevchuk@gmail.com>
2024-10-09 18:42:23 +0100

    Init project ToDo-List

---
+++ ToDo List/Resources/Constants/FilterEnableType.swift
@@ -0,0 +1,14 @@
+//
+//  Filters.swift
+//  ToDo List
+//
+//  Created by Artem Khlevchuk on 25.09.2024.
+//
+
+import Foundation
+
+enum FilterEnableType: String {
+    case all = "All"
+    case open = "Open"
+    case closed = "Closed"
+}
